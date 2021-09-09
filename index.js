7334c5fcd73b31537050530735043ed32b9d638e
Kyle Samuel <ksamuel92@gmail.com>
2021-09-09 11:48:03 -0400

    commit tree 17b146f1cf6150d2f26941825d23ac6789dca207

--- index.js
+++ index.js
@@ -1,1 +1,64 @@
 // Your code here
+
+function mapToNegativize(array){
+  const newArray = [];
+ for(const number of array){
+   const negativeNumber = number * -1
+    newArray.push(negativeNumber)
+ }
+ return newArray
+}
+
+function mapToNoChange(array){
+  const newArray = [];
+  for(const element of array){
+
+    newArray.push(element)}
+    return newArray
+  }
+
+  function mapToDouble(array){
+    const newArray = [];
+    for(const element of array){
+      const doubleNumber = element * 2
+      newArray.push(doubleNumber);
+    }
+    return newArray
+  }
+
+  function mapToSquare(array){
+    const newArray = [];
+    for(const element of array){
+      const squaredNumber = element ** 2
+      newArray.push(squaredNumber)
+    }
+    return newArray;
+  }
+
+  function reduceToTotal(array, startingPoint = 0){
+    let runningTotal = startingPoint
+    for (const element of array){
+      runningTotal += element 
+    }
+    return runningTotal
+  }
+
+  function reduceToAllTrue(array){
+    let truthyness = true
+    for(const element of array){
+      if (element === false) {
+        truthyness = false
+      }
+    }
+    return truthyness
+  }
+
+  function reduceToAnyTrue(array){
+    let truthyness = false
+    for (const element of array){
+      if (element === true) {
+        truthyness = true
+      }
+    }
+    return truthyness
+  }
